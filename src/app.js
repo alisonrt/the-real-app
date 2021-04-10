b37b28275da97703c6a925b817c306ab63a96040
Alison <alisonrt@gmail.com>
2021-04-09 17:32:10 -0700

    Date/Time display updated

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
-function displayTemp (response) {
+function dateFormat(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    if(minutes <= 9){
+        return `${day} ${hours}:0${minutes}`;
+    } else {
+          return `${day} ${hours}:${minutes}`;
+    }
+}
+
+function displayTemp(response) {
     let tempElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let conditionsElement = document.querySelector("#conditions");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#windspeed");
+    let dateElement = document.querySelector("#date");
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     conditionsElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = dateFormat(response.data.dt *1000);
     console.log(response.data);
 }
 

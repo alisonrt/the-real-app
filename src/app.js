6b4c3dbb440fa3befeff3c46b4cf88ce4fa6159d
Alison <alisonrt@gmail.com>
2021-04-09 18:48:22 -0700

    Added correct icon to API

--- src/app.js
+++ src/app.js
@@ -18,16 +18,20 @@ function displayTemp(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#windspeed");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     conditionsElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = dateFormat(response.data.dt *1000);
-    console.log(response.data);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+let city = "Seattle";
 let apiKey = "20f80f60d74acf5419e80528f290a5b9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
 axios.get(apiUrl).then(displayTemp);
